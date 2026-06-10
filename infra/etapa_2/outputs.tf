223bdc776e59c85fef99b3c02047875f37127c7c
Sergio <serg.castillo@duocuc.cl>
2026-06-10 14:32:29 -0400

    modificacion de carpetas infra

--- infra/etapa_2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "frontend_public_ip" {
-  value       = aws_instance.frontend.public_ip
-  description = "IP Publica del Servidor Frontend (Usa esto en el navegador)"
-}
-
-output "backend_public_ip" {
-  value       = aws_instance.backend.public_ip
-  description = "IP del Servidor Backend para que GitHub Actions se conecte por SSH"
-}
